42bfa836783cb40eb2ab0580fd118b6533c954c0
Viktoria Antonova <vickt.antonowa2016@yandex.ru>
2020-12-20 19:08:24 +0500

    refactor

--- script.js
+++ script.js
@@ -1,34 +1,32 @@
-let popup = document.querySelector('.popup'),
+let popup = document.querySelector('.popup-box'),
     form = popup.querySelector('.popup__container'),
     popupCloseButton = popup.querySelector('.popup__close-button'),
-    profileEditButton = document.querySelector('.profile__edit-button');
+    profileEditButton = document.querySelector('.profile__edit-button'),
+    InputTypeName = form.querySelector('.popup__input_type_name'),
+    InputTypeJob = form.querySelector('.popup__input_type_job'),
+    name = document.querySelector('.profile__name'),
+    job = document.querySelector('.profile__description');
 
 function openPopup() {
     popup.classList.add('popup_opened');
+
+    InputTypeName.value = name.textContent;
+    InputTypeJob.value = job.textContent;
 }    
 
 function closePopup() {
     popup.classList.remove('popup_opened');
 }
 
-profileEditButton.addEventListener('click', openPopup);
-popupCloseButton.addEventListener('click', closePopup);
-
 function formSubmit(evt) {
     evt.preventDefault();
-
-    let InputTypeName = form.querySelector('.popup__input_name'),
-        InputTypeJob = form.querySelector('.popup__input_job'),
-        name = document.querySelector('.profile__name'),
-        job = document.querySelector('.profile__description');
 
     name.textContent = InputTypeName.value;
     job.textContent = InputTypeJob.value;
 
-    InputTypeName.value = '';
-    InputTypeJob.value = '';
-
     closePopup();
 }    
 
+profileEditButton.addEventListener('click', openPopup);
+popupCloseButton.addEventListener('click', closePopup);
 form.addEventListener('submit', formSubmit);
