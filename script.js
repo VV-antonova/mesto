c876d382dae50081afcb184fe0c0f9dcaa85be9f
Viktoria Antonova <vickt.antonowa2016@yandex.ru>
2021-01-15 13:33:36 +0500

    feat:add function of opening picture popup

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const popups = document.querySelectorAll('.popup-box'),
     InputTypeJob = document.querySelector('.popup__input_type_job'), //инпут формы личных данных
     popupAddCard = document.querySelector('.popup-box_type_add-card'), //попап добавления карточки
     popupImage = document.querySelector('.popup-box_type_image'), //попап просмотра картинки
-    formProfile = document.querySelector('.popup-box_type_profile-form'),
+    formProfile = document.querySelector('.popup-box_type_profile-form'), //попап профиля
     formCard = popupAddCard.querySelector('.popup__container_type_place'), // форма добавления карточки
     InputTypePlaceName = formCard.querySelector('.popup__input_type_place-name'), //инпут формы добавления карточки
     InputTypeLink = formCard.querySelector('.popup__input_type_link'), //инпут формы добавления карточки
@@ -14,8 +14,7 @@ const popups = document.querySelectorAll('.popup-box'),
     profileName = document.querySelector('.profile__name'),
     job = document.querySelector('.profile__description'),
     cardsContainer = document.querySelector('.cards'),
-    cardTemplate = document.querySelector('#card-template').content,
-
+    
     initialCards = [
     {
         name: 'Архыз',
@@ -64,12 +63,15 @@ function closePopup() {
 
 //создание карточки
 function addCard (picLink, nameValue) {
-    
-    const cardElement = cardTemplate.cloneNode(true);
-    const busket = cardElement.querySelector('.cards__basket-button');
+    const cardTemplate = document.querySelector('#card-template').content,
+          cardElement = cardTemplate.cloneNode(true),
+          busket = cardElement.querySelector('.cards__basket-button');
+          
 
-    cardElement.querySelector('.cards__image').src = picLink;
+    cardPic = cardElement.querySelector('.cards__image');
+    cardPic.src = picLink;
     cardElement.querySelector('.cards__title').textContent = nameValue;
+
 
     cardElement.querySelector('.cards__like-button').addEventListener('click', evt => {
         evt.target.classList.toggle('cards__like-button_active');
@@ -80,6 +82,14 @@ function addCard (picLink, nameValue) {
         cardItem.remove();
     });  
 
+    cardPic.addEventListener('click', evt => {
+        if (evt.target === evt.currentTarget) {
+            document.querySelector('.popup__image').src = picLink;
+            document.querySelector('.popup__caption').textContent = nameValue;
+            openPopup(popupImage);
+        }
+    });
+    
     return cardElement;
 }
 
